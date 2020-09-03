046843982dcde08a6bc8bc863de60cfb27bd8ed8
sofia <eg-dem-1128.demidov@yandex.ru>
2020-09-03 20:55:17 +0300

    this is first commit

---
+++ bomjur.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var leftBorferWidth = 1; 
+{
+let second = 2;
+}
+const pi = 3.14;
+
+console.log(4/0);
+console.log('sting'*9);
+ 
+let flex = {
+    flip: "fonk",
+    false: 35  
+};
+
+console.log(flex.false);
+
+console.log(flex["flip"]);
+
+let arr = ["plum.png", "gg.jpg", "topset.gif"];
+
+console.log(arr[0]);
+
+//alert('hello world')
+ 
+//let answer = confirm("are u gay?");
+ 
+//console.log(answer);
+
+
+//let swag = +prompt("Do you 18 years old?", "Yes");
+
+//console.log(typeof(swag));
+
+//console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(5%2);
+
+let oop = true,
+    ooT = false;
+
+console.log( oop && ooT);    
+
+console.log( oop || ooT);
+
+console.log( oop && !ooT);
